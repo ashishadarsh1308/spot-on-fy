ec0974d5efa1da4fe7f2d666991ca5d0a8cfc9ba
ashishadarsh1308 <ashishadarsh319@gmail.com>
2026-02-05 02:51:32 +0530

    add JWT token

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./src/app')
 const connectDB = require('./db/db');
 
-// connectDB();
+connectDB(); 
 
 app.listen(3000, () => {
     console.log('server is running on PORT 3000')
